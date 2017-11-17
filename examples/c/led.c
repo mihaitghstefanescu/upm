27a2cee41a03b9fb5cce0c034adfe7c26509736b
mihais <mihai.t.gh.stefanescu@gmail.com>
2017-11-17 18:25:55 +0200

    OPC-UA Client-side simple LED example

--- examples/c/led.c
+++ examples/c/led.c
@@ -5,23 +5,35 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
 #include "led.h"
 
 #include "upm_utilities.h"
 
+int running = 1;
+
+void sig_handler(int signo)
+{
+    running = 0;
+}
+
 int main(void)
 {
-    led_context dev = led_init(2);
-    while(1){
+    signal(SIGINT, sig_handler);
+
+    led_context dev = led_init(100);
+
+    while(running){
         if(led_on(dev) != UPM_SUCCESS){
             printf("problem turning the LED on\n");
         }
-        upm_delay(1);
+        upm_delay(5);
         if(led_off(dev) != UPM_SUCCESS){
             printf("problem turning the LED off\n");
         }
-        upm_delay(1);
+        upm_delay(5);
     }
+
     led_close(dev);
     return 0;
 }
--- src/led/led.c
+++ src/led/led.c
@@ -26,6 +26,11 @@
  */
 #include "led.h"
 #include "upm_types.h"
+#include "open62541.h"
+
+UA_Client *client = NULL;
+UA_NodeId node_id;
+UA_StatusCode ua_status;
 
 led_context led_init(uint8_t pin){
     // make sure MRAA is initialized
@@ -48,23 +53,95 @@ led_context led_init(uint8_t pin){
     if (mraa_gpio_dir(dev->gpio, MRAA_GPIO_OUT) != MRAA_SUCCESS)
       return NULL;
 
+    /* Create open62541 client. */
+    if (client == NULL) {
+        client = UA_Client_new(UA_ClientConfig_standard);
+        /* TODO: Don't hardcode server address here. */
+        ua_status = UA_Client_connect(client, "opc.tcp://localhost:4840");
+        if (ua_status != UA_STATUSCODE_GOOD) {
+            printf("Could not start OPC-UA client\n");
+            UA_Client_delete(client);
+            client = NULL;
+            goto led_exit;
+        }
+
+        /* Create the OPC UA Node associated with this device. */
+        UA_VariableAttributes node_attr;
+        UA_VariableAttributes_init(&node_attr);
+        node_attr.displayName = UA_LOCALIZEDTEXT("en_US", "My UPM LED");
+        node_attr.description = UA_LOCALIZEDTEXT("en_US", "Sensor description goes here...");
+
+        /* NOTE: This is important - sensor data type. */
+        UA_Boolean button_pressed = UA_FALSE;
+        UA_Variant_setScalar(&node_attr.value, &button_pressed, &UA_TYPES[UA_TYPES_BOOLEAN]);
+        node_attr.dataType = UA_TYPES[UA_TYPES_BOOLEAN].typeId;
+
+        /* Register the node to the server. */
+        ua_status = UA_Client_addVariableNode(client,
+                                              UA_NODEID_NUMERIC(1, 0), // Random new node ID
+                                              UA_NODEID_NUMERIC(0, UA_NS0ID_OBJECTSFOLDER),
+                                              UA_NODEID_NUMERIC(0, UA_NS0ID_ORGANIZES),
+                                              UA_QUALIFIEDNAME(0, "LED Node"),
+                                              UA_NODEID_NULL, // no variable type
+                                              node_attr,
+                                              &node_id);
+
+        if (ua_status != UA_STATUSCODE_GOOD) {
+            printf("Could not create OPC-UA variable node.\n");
+            UA_Client_disconnect(client);
+            UA_Client_delete(client);
+            client = NULL;
+        }
+    }
+
+led_exit:
     return dev;
 }
 
 void led_close(led_context dev){
+
+    if (client) {
+        UA_Client_deleteNode(client, node_id, UA_TRUE);
+        UA_Client_disconnect(client);
+        UA_Client_delete(client);
+        client = NULL;
+    }
+
     free(dev);
 }
 
 upm_result_t led_on(led_context dev){
+    UA_Variant new_value;
+    UA_Variant_init(&new_value);
+    UA_Boolean read_value;
+
     if (mraa_gpio_write(dev->gpio, 1) != MRAA_SUCCESS)
         return UPM_ERROR_OPERATION_FAILED;
+
+    read_value = UA_TRUE;
+
+    if (client != NULL) {
+        UA_Variant_setScalar(&new_value, &read_value, &UA_TYPES[UA_TYPES_BOOLEAN]);
+        ua_status = UA_Client_writeValueAttribute(client, node_id, &new_value);
+    }
 
     return UPM_SUCCESS;
 }
 
 upm_result_t led_off(led_context dev){
+    UA_Variant new_value;
+    UA_Variant_init(&new_value);
+    UA_Boolean read_value;
+
     if (mraa_gpio_write(dev->gpio, 0) != MRAA_SUCCESS)
         return UPM_ERROR_OPERATION_FAILED;
+
+    read_value = UA_FALSE;
+
+    if (client != NULL) {
+        UA_Variant_setScalar(&new_value, &read_value, &UA_TYPES[UA_TYPES_BOOLEAN]);
+        ua_status = UA_Client_writeValueAttribute(client, node_id, &new_value);
+    }
 
     return UPM_SUCCESS;
 }
